3252916045faff85c37d93555ccbf4ceb96ac95e
Doğukan Köse <dogukankse4@gmail.com>
2019-12-07 00:30:56 +0300

    initial commit

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIManager : MonoBehaviour
+{
+    //fields
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject settingsMenu;
+    [SerializeField] private GameObject gameScreen;
+    
+    
+    public void PlayButtonListener()
+    {
+        mainMenu.SetActive(false);
+        gameScreen.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Slider slider;
+    [SerializeField] private float speed = 10f;
+    private float dir = 1;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        if (slider.value == 0f || slider.value == 1f)
+            dir *= -1;
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+                dir *= -1;
+        }
+        //pc controller for test
+        if (Input.GetMouseButtonDown(0))
+        {
+            dir *= -1;
+        }
+
+        slider.value += .1f * speed * dir * Time.smoothDeltaTime;
+    }
+}
